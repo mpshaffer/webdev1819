4ba913fcea5b99b188a140320b738560d2114f90
mpshaffer <43855134+mpshaffer@users.noreply.github.com>
2018-12-26 13:09:14 -0600

    Add files via upload

---
+++ Death/teacherlist.java
@@ -0,0 +1,15 @@
+public class teacherlist {
+	public static void teacherlist(String[] args)
+	{
+		System.out.println(Mr. Berg);
+		System.out.println(Mr. Kim);
+		System.out.println(Ms. Langchung);
+		System.out.println(Mr. Law);
+		System.out.println(Mr. Patel);
+		System.out.println(Ms. Remess);
+		System.out.println(Ms. Roscoe);
+		System.out.println(Mr. Solin);
+		System.out.println(Mr. Stone);
+		System.out.println(Mr. Vergara);
+		System.out.println(Ms. Woz);
+		System.out.println(Mr. Yan);
